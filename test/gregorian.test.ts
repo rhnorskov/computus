a92640c703ec76ec78f8cc7adfd5df7869ef3763
Rasmus Nørskov <rasmus@norskov.org>
2020-09-12 15:41:35 +0200

    refactor: added more tests

--- test/gregorian.test.ts
+++ test/gregorian.test.ts
@@ -8,4 +8,24 @@ describe("Gregorian Implementation Tests", () => {
   it("12 April 2020", () => {
     expect(gregorian(2020)).toEqual(new Date(2020, 4 - 1, 12));
   });
+
+  it("4 April 2021", () => {
+    expect(gregorian(2021)).toEqual(new Date(2021, 4 - 1, 4));
+  });
+
+  it("17 April 2022", () => {
+    expect(gregorian(2022)).toEqual(new Date(2022, 4 - 1, 17));
+  });
+
+  it("9 April 2023", () => {
+    expect(gregorian(2023)).toEqual(new Date(2023, 4 - 1, 9));
+  });
+
+  it("31 March 2024", () => {
+    expect(gregorian(2024)).toEqual(new Date(2024, 3 - 1, 31));
+  });
+
+  it("20 April 2025", () => {
+    expect(gregorian(2025)).toEqual(new Date(2025, 4 - 1, 20));
+  });
 });
